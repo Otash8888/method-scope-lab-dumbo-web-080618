4322184b5f70909d4bd8bfc21b26bdec28bbf6b3
Otabek Kamalov <otakam86@gmail.com>
2018-07-20 16:18:50 +0000

    automatically backed up by learn

--- lib/rescue_princess_peach.rb
+++ lib/rescue_princess_peach.rb
@@ -3,7 +3,7 @@ def rescue_princess_peach
   puts "Hooray! Mario has rescued Princess Peach."
 end
 
-
+#"Hooray! Mario has rescued Princess Peach
 
 
 ## Comment the below back in to see method scope in action!
